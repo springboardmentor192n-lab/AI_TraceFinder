43339b69f3b63d3defa04faa68b14f3fccaf930c
Athulya <athulyamajinuathulyamajinu@gmail.com>
2026-04-11 00:19:22 +0530

    Fix Gunicorn shutdown and memory issues

--- backend/app.py
+++ backend/app.py
@@ -224,4 +224,6 @@ def predict():
 # Local Development Entry Point
 # ---------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
